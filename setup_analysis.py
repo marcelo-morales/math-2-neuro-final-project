8f9eb1149ae5fde52419c7dc1880d5ba76c3badf
marcelomorales <lmoral10@jhu.edu>
2023-01-10 19:35:31 -0500

    try read csv

--- setup_analysis.py
+++ setup_analysis.py
@@ -1,51 +1,64 @@
 import json
 import csv
 import random
-
+import pandas as pd
 
 def set_up_healthcare():
+    
+    hospitalWorkerList = []
+    
     with open('data/healthcare_workers.csv', newline='') as csvfile:
         
         reader = csv.DictReader(csvfile)
+        
+        # reader = pd.read_csv(csvfile, engine = 'python')
+        
+        # pd.options.display.max_rows = 9999
 
-        text_file_song = open("text-datasets/song.txt", "w")
-        text_file_song.write('AudioId' + ' , ' + 'Title' + ' , ' + 'Artist' + 
-                 '\n')
-
-        text_file_album = open("text-datasets/album.txt", "w")
-        text_file_album.write('albumName' + ',' + 'artistName' + ',' + 'releaseYear' + ',' + 'label'
-                '\n')
-
-        text_file_artist = open("text-datasets/artist.txt", "w")
-        text_file_artist.write('Artist' + ' , ' + 'audioId' +
-                 '\n')
+        # df = pd.read_csv('data/healthcare_workers.csv', sep=",", encoding='cp1252')
+        
+        # print(df) 
+        
+        # df1 = pd.read_csv("https://raw.githubusercontent.com/tuyenhavan/Statistics/Dataset/World_Life_Expectancy.csv",
+        #           sep=";", encoding='cp1252')
 
         for row in reader:
-            current_song = Song(row['id'], row['title'], row['artist'])
-            text_file_song.write(str(current_song.audioId) + ' # ' + current_song.title + ' # ' + current_song.artist + ' # ' +
-                  '\n')
             
-            release_date = row['release_date']
-            release_year = release_date[0:4]
-            current_album = Album(row['album'], row['artist'], release_year, row['label'])
-            text_file_album.write(str(current_album.albumName) + ' # ' + current_album.artistName + ' # ' + current_album.releaseYear + ' # ' + current_album.label + '\n')
+            ageRow = row['Please indicate your age']
+            genderRow = row['Gender']
+            indianTerritoryRow = row['Please indicate which Indian State/Union territory you are currently residing in']
+            educationRow = row['Educational qualification']
+            maritalStatusRow = row['Marital status']
+            covidPositiveRow = row['Are you currently COVID-19 positive? ']
+            knowSomeoneCovidRow = row['Do you know any family member(s) or friend(s) who has/have tested positive for COVID-19?']
+            anxietyDisorderRow = row['Have you ever been diagnosed with any anxiety disorder by a health worker like a psychiatrist/psychologist/counselor before?']
+            adequateHospitalProtectionRow = row['The hospital has provided me with adequate personal protective equipment']
+            feelDizzyRow = row['"I felt dizzy, lightheaded, or faint, when I read or listened to news about the coronavirus."']
+            familyFearRow = row['My family expresses fear towards my work.']
             
-            current_artist = Artist(row['artist'], row['id'])
-            text_file_artist.write(str(current_artist.artistName) + ' * ' + current_artist.audioId + ' * ' + '\n')
-
-        text_file_song.close()
-        text_file_album.close()
-        text_file_artist.close()
+            myHopsitalWorker = HealthcareWorker(age=ageRow, gender=genderRow, indianTerritory=indianTerritoryRow, education=educationRow, maritalStatus=maritalStatusRow, covidPositive=covidPositiveRow,
+                                               knowSomeoneCovid=knowSomeoneCovidRow,anxietyDisorder=anxietyDisorderRow, feelDizzy=feelDizzyRow, 
+                                               adequateHospitalProtection=adequateHospitalProtectionRow, familyFear=familyFearRow)
+            
+            hospitalWorkerList.append(myHopsitalWorker)
+            
     
 #take out folllowing count
 class HealthcareWorker:
-  def __init__(self, username, personName, userBio, verified,  followerCount, likeNumber):
-    self.username = username
-    self.personName = personName
-    self.userBio = userBio
-    self.verified = verified
-    self.followerCount = followerCount
-    self.likeNumber = likeNumber
+  def __init__(self, age, gender, indianTerritory, education,  maritalStatus, covidPositive, knowSomeoneCovid, 
+               livingStatus, anxietyDisorder, familyFear, adequateHospitalProtection, feelDizzy):
+    self.age = age
+    self.gender = gender
+    self.indianTerritory = indianTerritory
+    self.education = education
+    self.maritalStatus = maritalStatus
+    self.covidPositive = covidPositive
+    self.knowSomeoneCovid = knowSomeoneCovid
+    self.livingStatus = livingStatus
+    self.anxietyDisorder = anxietyDisorder
+    self.familyFear = familyFear
+    self.adequateHospitalProtection = adequateHospitalProtection
+    self.feelDizzy = feelDizzy
     
 if __name__ == "__main__":
     set_up_healthcare()
